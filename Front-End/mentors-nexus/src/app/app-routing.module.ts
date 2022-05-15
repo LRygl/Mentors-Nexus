5bae4b63c948d1d52ccf59f5cfc866ffc2681781
Lubomír Rýgl <lubomir.rygl@aevi.com>
2022-05-15 21:55:40 +0200

    working on login

--- Front-End/mentors-nexus/src/app/app-routing.module.ts
+++ Front-End/mentors-nexus/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'user/management', component: UserComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front-End/mentors-nexus/src/app/app.module.ts
+++ Front-End/mentors-nexus/src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationGuard } from './guard/authentication.guard';
+import { AuthenticationService } from './service/authentication.service';
+import { UserService } from './service/user.service';
+
+import { FormsModule } from '@angular/forms';
+import { AuthInterceptor } from './interceptor/auth.interceptor';
+import { LoginComponent } from './component/login/login/login.component';
+import { UserComponent } from './component/user/user/user.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    AuthenticationService,
+    UserService,
+    AuthenticationGuard,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
